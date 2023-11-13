72eb8b5893105a5e6a7d955b547616f7e96668ff
Riefer-Ba <nick.riefer@web.de>
2023-11-13 17:26:37 +0100

    fix local startup - bump compile sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.emptyviewact"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.emptyviewact"
